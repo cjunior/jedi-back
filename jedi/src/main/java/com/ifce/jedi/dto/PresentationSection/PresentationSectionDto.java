96a338c67292f3d21372d11632eaf132512a7963
Samuel Esdras <esdrasamuel57@gmail.com>
2025-06-17 16:10:13 -0300

    Feito as refatorações da parte de PresentationSection, feita o put para trocar imagens

--- jedi/src/main/java/com/ifce/jedi/dto/PresentationSection/PresentationSectionDto.java
+++ jedi/src/main/java/com/ifce/jedi/dto/PresentationSection/PresentationSectionDto.java
@@ -1,9 +1,21 @@
 package com.ifce.jedi.dto.PresentationSection;
 
+import com.ifce.jedi.model.SecoesSite.PresentationSection;
+
 public record PresentationSectionDto(
-        String descricao,
-        String itemAzul,
-        String itemVerde,
-        String imageUrl,
-        String texto
-) {}
+        String title,
+        String description,
+        String firstStatistic,
+        String secondStatistic,
+        String imageUrl
+) {
+    public PresentationSectionDto(PresentationSection entity) {
+        this(
+                entity.getTitle(),
+                entity.getDescription(),
+                entity.getFirstStatistic(),
+                entity.getSecondStatistic(),
+                entity.getImageUrl()
+        );
+    }
+}
