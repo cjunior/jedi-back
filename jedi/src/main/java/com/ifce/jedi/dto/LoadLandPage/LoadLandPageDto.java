252337e84d65f4564ad47220c965d055f30e6468
Juliano Magalhaes Jurity <julianomagalhaes73@gmail.com>
2025-06-18 13:56:21 -0300

    feat: adicao de novos campos no metodo updateAll

--- jedi/src/main/java/com/ifce/jedi/dto/LoadLandPage/LoadLandPageDto.java
+++ jedi/src/main/java/com/ifce/jedi/dto/LoadLandPage/LoadLandPageDto.java
@@ -1,6 +1,8 @@
 package com.ifce.jedi.dto.LoadLandPage;
 
 import com.ifce.jedi.dto.Banner.BannerResponseDto;
+import com.ifce.jedi.dto.ContactUs.ContactUsResponseDto;
+import com.ifce.jedi.dto.Contents.ContentResponseDto;
 import com.ifce.jedi.dto.FaqSection.FaqSectionResponseDto;
 import com.ifce.jedi.dto.Header.HeaderResponseDto;
 import com.ifce.jedi.dto.PresentationSection.PresentationSectionResponseDto;
@@ -9,7 +11,9 @@ import com.ifce.jedi.dto.Team.TeamResponseDto;
 public record LoadLandPageDto(
         HeaderResponseDto headerResponseDto,
         BannerResponseDto bannerResponseDto,
-        TeamResponseDto teamResponseDto,
         PresentationSectionResponseDto presentationSectionResponseDto,
-        FaqSectionResponseDto faqSectionResponseDto  // Adicionado esta linha
+        TeamResponseDto teamResponseDto,
+        ContentResponseDto contentResponseDto,
+        FaqSectionResponseDto faqSectionResponseDto,
+        ContactUsResponseDto contactUsResponseDto
 ) {}
