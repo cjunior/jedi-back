d0fbb64c788cba744f8f5a6e146de702d2eebf03
Guilherme Pereira <guilherme.souza0996@gmail.com>
2025-07-21 08:54:34 -0300

    fix: trocando email para não dar conflito

--- jedi/src/main/resources/db/migration/V2__insert_admin.sql
+++ jedi/src/main/resources/db/migration/V2__insert_admin.sql
@@ -2,7 +2,7 @@ INSERT INTO users (
     id, login, password, name, created_at, role, photo_url
 ) VALUES (
              gen_random_uuid(),
-             'admin@example.com',
+             'admin@exemplo.com',
              '$2a$10$wnVRc9BG3rmcnvQU1BVZU.kEHLHExG2f/fiKlb9.Chmxs9NFxcNpq',
              'Admin',
              now(),
